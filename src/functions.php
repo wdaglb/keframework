1d46b70b689f0f8d5f1e138a5fd2cb763344d167
king east <1207877378@qq.com>
2017-07-20 00:38:34 +0800

    Signed-off-by: king east <1207877378@qq.com>

--- src/functions.php
+++ src/functions.php
@@ -4,10 +4,20 @@
  * User: King east
  * Site: http://cms.iydou.cn/
  */
+/**
+ * 获取当前域名
+ * @return string
+ */
 function get_domain(){
     $host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '');
     return $host;
 }
+
+/**
+ * 获取客户端IP
+ * @param bool $type
+ * @return mixed
+ */
 function get_client_ip($type=false){
     $type       =  $type ? 1 : 0;
     static $ip  =   NULL;
@@ -30,81 +40,4 @@ function get_client_ip($type=false){
     $long = sprintf("%u",ip2long($ip));
     $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
     return $ip[$type];
-}
-
-/**
- * 返回数组维度
- * @param $arr
- * @return mixed
- */
-function arrayLevel($arr){
-    $al = array(0);
-    function aL($arr,&$al,$level=0){
-        if(is_array($arr)){
-            $level++;
-            $al[] = $level;
-            foreach($arr as $v){
-                aL($v,$al,$level);
-            }
-        }
-    }
-    aL($arr,$al);
-    return max($al);
-}
-function cookie($name,$value='',$time=3600,$domain=''){
-    $pre=ke\Config::get('cookie.prefix');
-    if($value===''){
-        if(isset($_COOKIE[$pre.$name])){
-            return $_COOKIE[$pre.$name];
-        }else{
-            return '';
-        }
-    }elseif(is_null($value)){
-        setcookie($pre.$name,null,time()-$time,'/',$domain);
-        unset($_COOKIE[$pre.$name]);
-    }else{
-        setcookie($pre.$name,$value,time()+$time,'/',$domain);
-        $_COOKIE[$pre.$name]=$value;
-    }
-}
-function session($name,$value=''){
-    if(!isset($_SESSION)){session_start();}
-    $pre=\ke\Config::get('session.prefix');
-    if($value===''){
-        if(isset($_SESSION[$pre.$name])){
-            return $_SESSION[$pre.$name];
-        }else{
-            return '';
-        }
-    }elseif(is_null($value)){
-        if(strpos($name,'.')===false){
-            if(isset($pre)){
-                unset($_SESSION[ke\Config::get('session_pre')][$name]);
-            }else{
-                unset($_SESSION[$name]);
-            }
-        }else{
-            list($name1,$name2)=explode('.',$name);
-            if(isset($pre)){
-                unset($_SESSION[ke\Config::get('session_pre')][$name1][$name2]);
-            }else{
-                unset($_SESSION[$name1][$name2]);
-            }
-        }
-    }else{
-        if(strpos($name,'.')===false){
-            if(isset($pre)){
-                $_SESSION[ke\Config::get('session_pre')][$name]=$value;
-            }else{
-                $_SESSION[$name]=$value;
-            }
-        }else{
-            list($name1,$name2)=explode('.',$name);
-            if(isset($pre)){
-                $_SESSION[ke\Config::get('session_pre')][$name1][$name2]=$value;
-            }else{
-                $_SESSION[$name1][$name2]=$value;
-            }
-        }
-    }
 }
--- src/helper.php
+++ src/helper.php
@@ -29,3 +29,99 @@ function url($uri,$param=[])
 {
     return \ke\Route::url($uri,$param);
 }
+
+/**
+ * 获取GET数据
+ * @param string $key
+ * @return string
+ */
+function get($key='')
+{
+    if($key=='') return $_GET;
+    return isset($_GET[$key]) ? $_GET[$key] : '';
+}
+
+/**
+ * 获取POST数据
+ * @param string $key
+ * @return string
+ */
+function post($key='')
+{
+    if($key=='') return $_POST;
+    return isset($_POST[$key]) ? $_POST[$key] : '';
+}
+
+
+/**
+ * 设置/获取 COOKIE
+ * @param $name
+ * @param string $value
+ * @param int $time
+ * @param string $domain
+ * @return string
+ */
+function cookie($name,$value='',$time=3600,$domain=''){
+    $pre=ke\Config::get('cookie.prefix');
+    if($value===''){
+        if(isset($_COOKIE[$pre.$name])){
+            return $_COOKIE[$pre.$name];
+        }else{
+            return '';
+        }
+    }elseif(is_null($value)){
+        setcookie($pre.$name,null,time()-$time,'/',$domain);
+        unset($_COOKIE[$pre.$name]);
+    }else{
+        setcookie($pre.$name,$value,time()+$time,'/',$domain);
+        $_COOKIE[$pre.$name]=$value;
+    }
+}
+
+/**
+ * 设置/获取 SESSION
+ * @param $name
+ * @param string $value
+ * @return string
+ */
+function session($name,$value=''){
+    if(!isset($_SESSION)){session_start();}
+    $pre=\ke\Config::get('session.prefix');
+    if($value===''){
+        if(isset($_SESSION[$pre.$name])){
+            return $_SESSION[$pre.$name];
+        }else{
+            return '';
+        }
+    }elseif(is_null($value)){
+        if(strpos($name,'.')===false){
+            if(isset($pre)){
+                unset($_SESSION[ke\Config::get('session_pre')][$name]);
+            }else{
+                unset($_SESSION[$name]);
+            }
+        }else{
+            list($name1,$name2)=explode('.',$name);
+            if(isset($pre)){
+                unset($_SESSION[ke\Config::get('session_pre')][$name1][$name2]);
+            }else{
+                unset($_SESSION[$name1][$name2]);
+            }
+        }
+    }else{
+        if(strpos($name,'.')===false){
+            if(isset($pre)){
+                $_SESSION[ke\Config::get('session_pre')][$name]=$value;
+            }else{
+                $_SESSION[$name]=$value;
+            }
+        }else{
+            list($name1,$name2)=explode('.',$name);
+            if(isset($pre)){
+                $_SESSION[ke\Config::get('session_pre')][$name1][$name2]=$value;
+            }else{
+                $_SESSION[$name1][$name2]=$value;
+            }
+        }
+    }
+}
