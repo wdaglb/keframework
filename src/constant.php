f32aff48640067d9a70de5d7e6ffd1b9badc2507
King east <1207877378@qq.com>
2017-08-21 00:48:13 +0800

    Signed-off-by: King east <1207877378@qq.com>

--- src/constant.php
+++ src/constant.php
@@ -15,4 +15,4 @@ defined('RUNTIME_PATH') or define('RUNTIME_PATH',ROOT.'runtime/');
 defined('EXTENDS_PATH') or define('EXTENDS_PATH',ROOT.'extends/');
 
 $dir=dirname($_SERVER['SCRIPT_NAME']);
-defined('__WEB_PATH__') or define('__WEB_PATH__',($dir=='\\' ? '' : $dir).'/assets');
+defined('__WEB_PATH__') or define('__WEB_PATH__',($dir=='\\' || $dir=='/' ? '' : $dir).'/assets');
